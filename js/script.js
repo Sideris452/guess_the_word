5a5d02f570d2607dec0ead3e55e19ae5565efe69
D <167261788+Sideris452@users.noreply.github.com>
2024-08-20 14:21:55 -0700

    added js symbole and button functions

--- js/script.js
+++ js/script.js
@@ -1,10 +1,29 @@
 const guessedLettersElemnt = document.querySelector(".guessed-letters");
 const guessLetterButton = document.querySelector(".guess");
 const letterInput = document.querySelector(".letter");
-const wordinProgress = document.querySelector(".word-in-progress");
+const wordInProgress = document.querySelector(".word-in-progress");
 const remainingGuessesElement = document.querySelector(".remaining");
 const remainingGuessesSpan = document.querySelector(".remaining span");
 const message = document.querySelector(".message");
 const playAgainButton = document.querySelector(".play-again");
 
 const word = "magnolia";
+
+// display circle symbol as placeholder for chosen word's letters
+const placeholder = function(word) {
+    const placeholderLetters = [];
+    for (const letter of word) {
+        console.log(letter);
+        placeholderLetters.push("●");
+    }
+    wordInProgress.innerText = placeholderLetters.join("");
+};
+
+placeholder(word);
+
+guessLetterButton.addEventListener("click", function(e) {
+    e.preventDefault();
+    const guess = letterInput.ariaValue;
+    console.log(guess);
+    letterInput.value = "";
+});
