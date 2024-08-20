c9b89a67850f467c4a3620580032c0841508f030
D <167261788+Sideris452@users.noreply.github.com>
2024-08-20 14:10:22 -0700

    added js global variables

---
+++ js/script.js
@@ -0,0 +1,10 @@
+const guessedLettersElemnt = document.querySelector(".guessed-letters");
+const guessLetterButton = document.querySelector(".guess");
+const letterInput = document.querySelector(".letter");
+const wordinProgress = document.querySelector(".word-in-progress");
+const remainingGuessesElement = document.querySelector(".remaining");
+const remainingGuessesSpan = document.querySelector(".remaining span");
+const message = document.querySelector(".message");
+const playAgainButton = document.querySelector(".play-again");
+
+const word = "magnolia";
